af4ed8e29d5a1ff957b9f89fe15a2c96f9a1354e
Chris Burn <chrisburn@fastmail.net>
2023-06-26 09:21:26 +0000

    added ser2sock

--- cgate/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-RESTART=126
-while [ $RESTART -eq 126 ]
-do
-        java -Djava.library.path=. -jar cgate.jar -c /config/CGate/config/C-GateConfig.txt
-        RESTART=$?
-done
